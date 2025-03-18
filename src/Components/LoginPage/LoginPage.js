384325dadd011105a4d75ff41a6e214004fca3bd
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-03-19 03:59:23 +0530

    Initialize project structure: Add components folder and setup base code

---
+++ src/Components/LoginPage/LoginPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class LoginPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { }
+    }
+    render() {
+        return (
+            <div className="App">Hello React</div>
+        )
+    }
+}
+
+export default LoginPage;
