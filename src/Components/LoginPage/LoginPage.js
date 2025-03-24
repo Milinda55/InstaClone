303640b8354a0d7e1ec6b0f87c5fa0115572d645
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-03-24 23:20:07 +0530

    Improve the few styles

--- src/Components/LoginPage/LoginPage.js
+++ src/Components/LoginPage/LoginPage.js
@@ -4,6 +4,8 @@ import './LoginPage.css';
 import inst_image from "../../images/9364675fb26a.svg";
 import inst_logo from "../../images/logoinsta.png";
 import fb_logo from "../../images/fb.png";
+import appstore from '../../images/app.png';
+import playstore from '../../images/play.png';
 
 
 class LoginPage extends Component {
@@ -11,6 +13,15 @@ class LoginPage extends Component {
         super(props);
         this.state = { }
     }
+
+    changeLogin=()=>{
+        if(this.state.isLogin)
+            this.setState({isLogin: false});
+        else
+            this.setState({isLogin: true});
+    }
+
+
     render() {
         return (
             <div>
@@ -19,15 +30,18 @@ class LoginPage extends Component {
                     <Grid item xs={6}>
                         <div className="login-page-main">
                             <div>
-                                <img src={inst_image} width="350" alt={"instagram"}/>
+                                <img src={inst_image} width="450" alt={"instagram"}/>
                             </div>
                         <div>
                         <div className="loginPage_rightComponent">
                             <img className="loginPage_logo" src={inst_logo} alt={"logo"}/>
                             <div className="loginPage_signin">
+
                                 <input className="loginPage_text" type="text" placeholder="Phone number, username or email"/>
                                 <input className="loginPage_text" type="password" placeholder="Password"/>
                                 <button className="login_button">Sign In</button>
+
+
                             </div>
 
                             <div className="loginPage_ordiv">
@@ -42,15 +56,22 @@ class LoginPage extends Component {
                             <div className="login_forgot"> Forgot password?</div>
 
                             <div className="loginpage_signupoption">
-                                {
-                                    this.state.isLogin ?
-                                        <div className="loginPage_signin">
-                                            Don't have an account? <span onClick={this.changeLogin} style={{ "fontWeight":"bold", "color":"#0395F6"}}>Sign up</span>
-                                        </div> :
-                                        <div className="loginPage_signup">
-                                            Have an account? <span onClick={this.changeLogin}  style={{ "fontWeight":"bold", "color":"#0395F6"}}>Sign in</span>
-                                        </div>
-                                }
+                                <div className="loginPage_signin">
+                                    Don't have an account? <span onClick={this.changeLogin} style={{ "fontWeight":"bold", "color":"#0395F6"}}>Sign up</span>
+                                </div>
+                                {/*<div className="loginPage_signup">*/}
+                                {/*    Have an account? <span onClick={this.changeLogin}  style={{ "fontWeight":"bold", "color":"#0395F6"}}>Sign in</span>*/}
+                                {/*</div>*/}
+                            </div>
+
+                            <div className="loginPage__downloadSection">
+                                <div>
+                                    Get the app.
+                                </div>
+                                <div className="loginPage__option">
+                                    <img className="loginPage_dwimg" src={appstore} width="136px" />
+                                    <img className="loginPage_dwimg" src={playstore} width="136px" />
+                                </div>
                             </div>
 
                         </div>
